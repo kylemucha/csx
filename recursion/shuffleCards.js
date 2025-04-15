fe310c2384591580c2400a80cf49633f054f02c2
kylemucha <kylemuchaswe@gmail.com>
2025-04-15 16:47:48 -0700

    Challenge: shuffleCards (in progress)
    Did some work on the solution, my output is not perfect but has some parts to the logic that seems correct, plan on spending more time on this.

---
+++ recursion/shuffleCards.js
@@ -0,0 +1,59 @@
+// You are creating a card game application with your friend.
+
+// She already wrote a function that divides the deck of cards into top and bottom halves, but needs help writing a function that shuffles the two halves together again.
+
+// Challenge: shuffleCards  
+
+// Write a function that takes two arrays as inputs, representing the top and bottom halves of a deck of cards, and shuffles them together. The function will return a single array containing the elements from both input arrays interleaved, like so:
+
+// the first element should be the first element of the first input array,
+
+// the second element should be the first element of the second input array,
+
+// the third element should be the second element of the first input array,
+
+// the fourth element should be the second element of the second array,
+
+// and so on.
+
+// The arrays may be of different lengths. After interleaving the elements of the input arrays, any remaining elements should be appended to the end of the array.
+
+// This problem can be solved in many ways, but try to solve it with recursion!
+
+
+
+function shuffleCards(topHalf, bottomHalf, thCount = 0, bhCount = 0, shuffled = []) {
+    // YOUR CODE HERE
+    if(thCount === topHalf.length){
+      shuffled.push(bottomHalf);
+      return shuffled;
+    }
+    if(bhCount === bottomHalf.length){
+      shuffled.push(topHalf);
+      return shuffled;
+    }
+    
+    shuffled.push(topHalf[thCount]);
+    shuffled.push(bottomHalf[bhCount]);
+    
+  
+     
+    
+    return shuffleCards(topHalf, bottomHalf, thCount + 1, bhCount + 1, shuffled);
+    
+  }
+  
+  
+  
+  // UNCOMMENT TO TEST YOUR WORK
+  const topHalf = ['Queen of Diamonds', 'Five of Hearts', 'Ace of Spades', 'Eight of Clubs'];
+  const bottomHalf = ['Jack of Hearts', 'Ten of Spades'];
+  console.log(shuffleCards(topHalf, bottomHalf));
+    /*-> ['Queen of Diamonds',
+          'Jack of Hearts',
+          'Five of Hearts',
+          'Ten of Spades',
+          'Ace of Spades',
+          'Eight of Clubs',
+        ]
+    */
