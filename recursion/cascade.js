147b5144982e489e5995b5a64f704ecc5ff8ec60
kylemucha <kylemuchaswe@gmail.com>
2025-04-17 21:05:01 -0700

    Challenge: Cascade (in progress)
    I am planning on trying to solve this more but am having trouble even coming up with a naive solution. Will think about this more

---
+++ recursion/cascade.js
@@ -0,0 +1,36 @@
+// Challenge: Cascade  
+
+// Write a function cascade that takes a positive integer and prints a cascade of this integer. 
+// Hint - this very challenging problem can be solved with and without string manipulation!
+
+// cascade(12345) should print
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+function cascade(number) {
+	// Your code here!
+  console.log(number);
+  newNumber = number.replace()
+  console.log(number);
+}
+
+
+
+// // Uncomment to test your work!
+cascade(111)
+// // should print
+// /*
+// 111
+// 11
+// 1
+// 11
+// 111
+// */
