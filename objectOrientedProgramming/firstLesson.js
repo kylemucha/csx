c83a7651a7f336a38affa28b9d70a2f9c6c60c25
kylemucha <kylemuchaswe@gmail.com>
2025-04-23 16:47:51 -0700

    Object Oriented Programming (learning)
    finishing up readings and articles

--- objectOrientedProgramming/firstLesson.js
+++ objectOrientedProgramming/firstLesson.js
@@ -1,3 +1,5 @@
 //here is the first lesson of object oriented programming.
 
 //currently watching videos and reading the information on the website.
+
+// and reading about the positives and trade offs of using Object Oritented Programming.
