28e60b3b53dd2df8f69d8918d90502a0c7696f78
kylemucha <kylemuchaswe@gmail.com>
2025-04-28 15:46:49 -0700

    OOP: Challenge - personFromPersonStore
    Completed challenge ragarding using Object.create

---
+++ OOP/personFromPersonStore.js
@@ -0,0 +1,28 @@
+// Challenge: personFromPersonStore
+
+
+// Create a function personFromPersonStore that takes as input a name and an age. 
+// When called, the function will create person objects using the 
+// Object.create method on the personStore object.
+
+const personStore = {
+    greet: function() {
+      console.log('hello');
+    }
+  }
+  
+  function personFromPersonStore(name, age) {
+      // add code here
+    const person = Object.create(personStore);
+    person.name = name;
+    person.age = age;
+    
+    return person;
+  }
+  
+  const sandra = personFromPersonStore('Sandra', 26);
+  
+  // Uncomment these lines to check your work!
+//   console.log(sandra.name); // -> Logs 'Sandra'
+//   console.log(sandra.age); // -> Logs 26
+//   sandra.greet(); // -> Logs 'hello'
