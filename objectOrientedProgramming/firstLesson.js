e7026b9231d25c1e23c8d5a58afa171e71648818
kylemucha <kylemuchaswe@gmail.com>
2025-04-28 15:34:19 -0700

    OOP: Challenge - makePerson
    Completed challenge.

--- objectOrientedProgramming/firstLesson.js
+++
@@ -1,5 +0,0 @@
-//here is the first lesson of object oriented programming.
-
-//currently watching videos and reading the information on the website.
-
-// and reading about the positives and trade offs of using Object Oritented Programming
